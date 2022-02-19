a2b909858e6a651b824b37eded0db71859ed62b8
varunsuravajhela <vsuravajhela85@gmail.com>
2022-02-18 17:18:54 -0800

    Started the project!

---
+++ doinmypart/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header className='mainHeader'>Doin' My Part</header>
+      <h1>What I have to do next is find the right fonts and add buttons, include spacing as well</h1>
+    </div>
+  );
+}
+
+export default App;
