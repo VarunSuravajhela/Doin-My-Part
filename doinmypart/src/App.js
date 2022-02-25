e3f204970e1a29b73021623c2f93e3f109072efa
varunsuravajhela <vsuravajhela85@gmail.com>
2022-02-24 21:59:32 -0800

    Outline of home page

--- doinmypart/src/App.js
+++ doinmypart/src/App.js
@@ -1,11 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import "@fontsource/spicy-rice"
 
 function App() {
   return (
     <div>
-      <header className='mainHeader'>Doin' My Part</header>
-      <h1>What I have to do next is find the right fonts and add buttons, include spacing as well</h1>
+      <header className='mainHeader'>Doin' My Part!</header>
+      <div className='envFactDiv'>
+        <h1 className='envFact'>Environment Fact of the Day!</h1>
+        <h2 className='envFactBody'>About 1/3 of landfill material is made up of packaging</h2>
+      </div>
+      <div className='itemEntryDiv'>
+        <h1 className='itemEntryTitle'>How much have you recycled today?</h1>
+        <h2 className="itemEntry">Item: </h2>
+      </div>
+      <div className='itemEntryDiv'>
+        <h1 className='itemEntryTitle'>How much have you composted today?</h1>
+        <h2 className="itemEntry">Item: </h2>
+      </div>
+      <div className='buttonDiv'>
+        <button className='calcButton'>Calculate your impact!</button>
+      </div>
     </div>
   );
 }
